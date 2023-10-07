1c0b82f28c2e76c8194649bcb0271480c06ccdbb
Kartik-appsdeployer <76987281+KartikKulshreshtha@users.noreply.github.com>
2023-10-07 15:05:28 +0530

    Changed the Nav and added the dropdown menus for About and Services with there same functionalities

--- index.js
+++ index.js
@@ -20,3 +20,75 @@ function rotateCarousel() {
 
 setInterval(rotateCarousel, 3000);
 showImages(currentIndex);
+
+
+// Hovering functionality
+function handleHover(){
+    const targetElement = document.getElementById("dropdown-about");
+    targetElement.style.display = "flex";
+}
+
+const Element = document.getElementById("about")
+Element.addEventListener("mouseover", handleHover);
+
+function handleMouseOut() {
+    const targetElement = document.getElementById("dropdown-about");
+    targetElement.style.display = "none";
+}
+
+const elementToHover = document.getElementById("about");
+
+elementToHover.addEventListener("mouseout", handleMouseOut);
+
+
+function handleHover1(){
+    const targetElement = document.getElementById("dropdown-services");
+    targetElement.style.display = "flex";
+}
+
+const Element1 = document.getElementById("services")
+Element1.addEventListener("mouseover", handleHover1);
+
+function handleMouseOut1() {
+    const targetElement = document.getElementById("dropdown-services");
+    targetElement.style.display = "none";
+}
+
+const elementToHover1 = document.getElementById("services");
+
+elementToHover1.addEventListener("mouseout", handleMouseOut1);
+
+
+function handleChange1(){
+    changeElement2.classList.remove("active");
+    changeElement3.classList.remove("active");
+    changeElement1.classList.add("active")
+    document.getElementById("all-element-content1").style.display = "block"
+    document.getElementById("all-element-content2").style.display = "none"
+    document.getElementById("all-element-content3").style.display = "none"
+}
+function handleChange2(){
+    changeElement1.classList.remove("active");
+    changeElement3.classList.remove("active");
+    changeElement2.classList.add("active")
+
+    document.getElementById("all-element-content1").style.display = "none"
+    document.getElementById("all-element-content2").style.display = "block"
+    document.getElementById("all-element-content3").style.display = "none"
+}
+function handleChange3(){
+    changeElement2.classList.remove("active");
+    changeElement1.classList.remove("active");
+    changeElement3.classList.add("active")
+
+    document.getElementById("all-element-content1").style.display = "none"
+    document.getElementById("all-element-content2").style.display = "none"
+    document.getElementById("all-element-content3").style.display = "block"
+}
+
+const changeElement1 = document.getElementById("web");
+changeElement1.addEventListener("click", handleChange1)
+const changeElement2 = document.getElementById("digital");
+changeElement2.addEventListener("click", handleChange2)
+const changeElement3 = document.getElementById("reseller");
+changeElement3.addEventListener("click", handleChange3)
